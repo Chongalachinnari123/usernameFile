19b6ae8a741bd8ddfdbecdbeb2cbc72bc047144d
Chongalachinnari123 <138453951+Chongalachinnari123@users.noreply.github.com>
2023-07-03 16:39:50 +0530

    Add files via upload

---
+++ file.py
@@ -0,0 +1,6 @@
+name = ""
+password = ""
+while name != "chinnari" or password != "123":
+    name = input("enter the name:")
+    password = input("enter the password:")
+print("you are authorized person")    
---
+++ text.py
@@ -0,0 +1,6 @@
+name = input("enter name:")
+nickname = input("enter nickname:")
+while name == "chinnari" or nickname == "chinna":
+    result = "she is chinnari"
+print(result)
+        
